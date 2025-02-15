c356693ddf4cff24c291f5dea9ae0b5f72cfa2e0
CDevv <cdevyt@gmail.com>
2025-02-15 11:28:46 +0200

    Add project files.

---
+++ BinarySearchTree/BinaryTree.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    public class BinaryTree
+    {
+        public Node Root { get; set; }
+
+        public BinaryTree(Node root)
+        {
+            Root = root;
+        }
+
+        public void Print()
+        {
+            Print(Root, 0);
+        }
+
+        private const int Distance = 10;
+        private void Print(Node root, int space)
+        {
+            if (root == null)
+            {
+                return;
+            }
+
+            space += Distance;
+            Print(root.Right, space);
+            Console.Write("\n");
+            for (int i = Distance; i < space; i++)
+            {
+                Console.Write(" ");               
+            }
+            Console.Write($"{root.Value} \n");
+            Print(root.Left, space);
+        }
+
+        public bool IfNodeExists(Node node, int val)
+        {
+            if (node == null)
+            {
+                return false;
+            }
+            if (node.Value == val)
+            {
+                return true;
+            }
+
+            if (IfNodeExists(node.Left, val))
+            {
+                return true;
+            }
+
+            return IfNodeExists(node.Right, val);
+        }
+
+        public void Insert(int val)
+        {
+            Insert(Root, val);
+        }
+
+        private Node Insert(Node node, int val)
+        {
+            if (node == null)
+            {
+                return new Node(val);
+            }
+
+            if (val < node.Value)
+            {
+                node.Left = Insert(node.Left, val);
+            }
+            else
+            {
+                node.Right = Insert(node.Right, val);
+            }
+
+            return node;
+        }
+    }
+}
---
+++ BinarySearchTree/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace BinarySearchTree
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Node root = new Node(8);
+
+            BinaryTree tree = new BinaryTree(root);
+            tree.Root.Left = new Node(3);
+            tree.Root.Left.Left = new Node(1);
+            tree.Root.Left.Right = new Node(6);
+            tree.Root.Left.Right.Left = new Node(4);
+            tree.Root.Left.Right.Right = new Node(7);
+            tree.Root.Right = new Node(10);
+            tree.Root.Right.Left = new Node(14);
+            tree.Root.Right.Left.Left = new Node(13);
+
+            tree.Insert(11);
+
+            tree.Print();
+            Console.WriteLine(tree.IfNodeExists(tree.Root, 8));
+        }
+    }
+}
